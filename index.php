ce5b24ada59fc07b657a2543c4331344a5e12979
Rovic Munoz <72641945+IceCube12@users.noreply.github.com>
2024-08-31 05:28:54 +0000

    Added basic PHP script

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<?php
+echo "The date is" . date("Y/m/d"). "br";
+echo "The time is" . date("h:i:sa");
+?>
+
+<html>
+<body>
+
+<form action="funtions.php" method="POST">
+Name: <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
